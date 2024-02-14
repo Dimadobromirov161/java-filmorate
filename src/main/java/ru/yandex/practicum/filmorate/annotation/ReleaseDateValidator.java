797f6ed7ea11d232fc87d2ff28b8d3b00b162753
Dimadobromirov161 <141020017+Dimadobromirov161@users.noreply.github.com>
2024-02-15 00:15:43 +0300

    Спринт 9

--- src/main/java/ru/yandex/practicum/filmorate/annotation/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotation/ReleaseDateValidator.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.annotation;1
+package ru.yandex.practicum.filmorate.annotation;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;

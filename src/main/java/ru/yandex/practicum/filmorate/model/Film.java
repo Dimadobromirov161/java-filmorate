4da7dbeb7a60404c7864a82a17071d511b8817cc
Dimadobromirov161 <141020017+Dimadobromirov161@users.noreply.github.com>
2024-02-20 23:40:50 +0300

    feat: добавить дополнительные аннотации для валидации

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.Length;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import javax.validation.constraints.Min;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class Film {
+
+    int id;
+    @NonNull
+    String name;
+    @NonNull @Length(min = 1, max = 200, message = "Описание фильма не должно превышать 200 символов.")
+    String description;
+    @NonNull @PastOrPresent(message = "Дата не может быть в будущем")
+    LocalDate releaseDate;
+    @NonNull @Min(value = 0, message = "Продолжительность фильма должна быть положительной")
+    Integer duration;
 }

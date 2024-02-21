fc48dcfb3336d481599fe9fb81683dfce3f018de
Dimadobromirov161 <141020017+Dimadobromirov161@users.noreply.github.com>
2024-02-21 23:25:47 +0300

    UserRepository.java

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserRepository.java
@@ -1,27 +1,27 @@
 package ru.yandex.practicum.filmorate.dao;
 
-import ru.yandex.practicum.filmorate.model.User;
+        import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.List;
+        import java.util.Map;
 
 public class UserRepository {
 
     private final Map<Integer, User> users = new HashMap<>();
     private int idNumber = 0;
 
-    public void add (User user) {
+    public void add(User user) {
         user.setId(++idNumber);
         users.put(idNumber, user);
     }
 
-    public void update (User user) {
+    public void update(User user) {
         users.put(user.getId(), user);
     }
 
-    public User getById (int id) {
+    public User getById(int id) {
         return users.get(id);
     }
 
@@ -29,7 +29,7 @@ public class UserRepository {
         return new ArrayList<>(users.values());
     }
 
-    public Map<Integer,User> getAll() {
+    public Map<Integer, User> getAll() {
         return users;
     }
 }

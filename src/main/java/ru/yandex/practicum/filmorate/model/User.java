8a40d9e7bf04fa54d38bf74a00613b09a4ca2f0c
Dimadobromirov161 <141020017+Dimadobromirov161@users.noreply.github.com>
2024-02-21 23:29:07 +0300

    User.java

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,14 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.NonNull;
+        import lombok.Builder;
+        import lombok.Data;
+        import lombok.NonNull;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Pattern;
-import java.time.LocalDate;
+        import javax.validation.constraints.Email;
+        import javax.validation.constraints.NotBlank;
+        import javax.validation.constraints.PastOrPresent;
+        import javax.validation.constraints.Pattern;
+        import java.time.LocalDate;
 
 @Data
 @Builder
@@ -16,11 +16,14 @@ public class User {
 
     int id;
     String name;
-    @NonNull @Email(message = "Введено некорректное значение электронной почты.")
+    @NonNull
+    @Email(message = "Введено некорректное значение электронной почты.")
     String email;
-    @NonNull @NotBlank(message = "Логин не может быть пустым")
-    @Pattern(regexp ="\\S*", message = "Логин не может содержать пробелы.")
+    @NonNull
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "\\S*", message = "Логин не может содержать пробелы.")
     String login;
-    @NonNull  @PastOrPresent(message = "Дата не может быть в будущем")
+    @NonNull
+    @PastOrPresent(message = "Дата не может быть в будущем")
     LocalDate birthday;
 }

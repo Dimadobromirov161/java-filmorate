66a331d8a6cdf7fb715cd6245cf734b285bff3ee
Dimadobromirov161 <141020017+Dimadobromirov161@users.noreply.github.com>
2024-02-21 23:21:46 +0300

    FilmRepository.java

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmRepository.java
@@ -1,27 +1,27 @@
 package ru.yandex.practicum.filmorate.dao;
 
-import ru.yandex.practicum.filmorate.model.Film;
+        import ru.yandex.practicum.filmorate.model.Film;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.List;
+        import java.util.Map;
 
 public class FilmRepository {
 
     private final Map<Integer, Film> films = new HashMap<>();
     private int idNumber = 0;
 
-    public void add (Film film) {
+    public void add(Film film) {
         film.setId(++idNumber);
         films.put(idNumber, film);
     }
 
-    public void update (Film film) {
+    public void update(Film film) {
         films.put(film.getId(), film);
     }
 
-    public Film getById (int id) {
+    public Film getById(int id) {
         return films.get(id);
     }
 
@@ -29,7 +29,7 @@ public class FilmRepository {
         return new ArrayList<>(films.values());
     }
 
-    public Map<Integer,Film> getAll() {
+    public Map<Integer, Film> getAll() {
         return films;
     }
 }

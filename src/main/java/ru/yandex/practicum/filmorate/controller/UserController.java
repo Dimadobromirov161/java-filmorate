2617f88c7863890ca4b16b05a676ce177794432c
Dimadobromirov161 <141020017+Dimadobromirov161@users.noreply.github.com>
2024-02-26 23:22:32 +0300

    UserController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controllers;
 
 import org.slf4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.*;
@@ -12,32 +13,43 @@ import ru.yandex.practicum.filmorate.validators.UserValidator;
 import javax.validation.Valid;
 import java.util.List;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private static int generatorId = 0;
+    private final Map<Integer, User> users = new HashMap<>();
 
-    private final UserRepository repository = new UserRepository();
-    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    @GetMapping
+    public List<User> findAll() {
+        return new ArrayList<>(users.values());
+    }
 
-    @PostMapping()
-    public User addUser(@Valid  @RequestBody User user) {
-        UserValidator.validate(user, repository.getAll(), HttpMethod.POST);
-        log.info("object " + user + " passed validation. returns object");
-        repository.add(user);
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+        validate(user);
+        user.setId(++generatorId);
+        users.put(user.getId(), user);
+        log.debug("Пользователь {} создан.", user.getLogin());
         return user;
     }
 
-    @PutMapping()
-    public User updateUser(@Valid @RequestBody User user) {
-        UserValidator.validate(user, repository.getAll(), HttpMethod.PUT);
-        log.info("object " + user + " passed validation. update and returns object");
-        repository.update(user);
+    @PutMapping
+    public User update(@Valid @RequestBody User user) {
+        int id = user.getId();
+        if (!users.containsKey(id)) {
+            log.debug("Пользователь не найден.");
+            throw new ValidationException("Пользователь не найден.");
+        }
+        validate(user);
+        users.put(id, user);
+        log.debug("Пользователь {} обновлен.", user.getLogin());
         return user;
     }
 
-    @GetMapping()
-    public List<User> getUsers() {
-        return repository.getAllInList();
+    public void validate(User user) {
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
     }
 }

c2f0975a637e640e1edaca86a3dcd0aecb69e0fb
Dimadobromirov161 <141020017+Dimadobromirov161@users.noreply.github.com>
2024-02-28 00:06:54 +0300

    UserController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,16 +1,16 @@
 package ru.yandex.practicum.filmorate.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.dao.UserRepository;;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.validators.UserValidator;
+        import org.slf4j.Logger;
+        import org.slf4j.LoggerFactory;
+        import org.springframework.http.HttpMethod;
+        import org.springframework.web.bind.annotation.*;
+        import ru.yandex.practicum.filmorate.dao.UserRepository;;
+        import ru.yandex.practicum.filmorate.model.User;
+        import ru.yandex.practicum.filmorate.validators.UserValidator;
 
 
-import javax.validation.Valid;
-import java.util.List;
+        import javax.validation.Valid;
+        import java.util.List;
 
 
 @RestController
@@ -21,7 +21,7 @@ public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @PostMapping()
-    public User addUser(@Valid  @RequestBody User user) {
+    public User addUser(@Valid @RequestBody User user) {
         UserValidator.validate(user, repository.getAll(), HttpMethod.POST);
         log.info("object " + user + " passed validation. returns object");
         repository.add(user);

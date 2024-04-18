cb927249c9b1d692dd78f1e43884c4d3822ef6a6
Dimadobromirov161 <141020017+Dimadobromirov161@users.noreply.github.com>
2024-04-18 21:55:20 +0300

    Film.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,30 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+        import lombok.Data;
+        import javax.validation.constraints.NotBlank;
+        import javax.validation.constraints.NotNull;
+        import javax.validation.constraints.Positive;
+        import javax.validation.constraints.Size;
+        import java.time.LocalDate;
+        import java.util.HashSet;
+        import java.util.Set;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+@Data
 public class Film {
+    private final Set<Integer> likes = new HashSet<>();
+    private int id;
+    @NotBlank
+    @NotNull
+    String name;
+    @NotNull
+    @Size(min = 1, max = 100)
+    String description;
+    @NotNull
+    LocalDate releaseDate;
+    @Positive
+    Integer duration;
+
+    public int getcountLikes() {
+        return likes.size();
+    }
 }

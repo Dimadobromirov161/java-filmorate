814f5fca721ee4239083204b95401fce98b2f696
Dimadobromirov161 <141020017+Dimadobromirov161@users.noreply.github.com>
2024-04-18 21:38:45 +0300

    InMemoryFilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/FriendStorage.java
+++
@@ -1,9 +0,0 @@
-public interface FriendStorage {
-    void addFriend(int userId, int friendId);
-
-    void removeFriend(int userId, int friendId);
-
-    List<User> findAllFriends(int id);
-
-    List<User> findCommonFriends(int id, int otherId);
-}

453c2c83be9d9c4e0d0ca4a59e3a4df40077b6f8
Dimadobromirov161 <141020017+Dimadobromirov161@users.noreply.github.com>
2024-04-18 21:56:49 +0300

    User.java

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,26 @@
+package ru.yandex.practicum.filmorate.model;
+
+        import lombok.Data;
+        import javax.validation.constraints.Email;
+        import javax.validation.constraints.NotBlank;
+        import javax.validation.constraints.NotNull;
+        import javax.validation.constraints.PastOrPresent;
+        import java.time.LocalDate;
+        import java.util.HashSet;
+        import java.util.Set;
+
+@Data
+public class User {
+    private final Set<User> friends = new HashSet<>();
+    int id;
+    @NotNull
+    @Email
+    String email;
+    @NotNull
+    @NotBlank
+    String login;
+    String name;
+    @NotNull
+    @PastOrPresent(message = "The birthday cannot be in the future tense.")
+    LocalDate birthday;
+}
